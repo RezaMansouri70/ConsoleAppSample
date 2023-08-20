cd22f366842fca468c2e51c5e5aeae1132f375b3
Reza Mansouri <Reza.Mansouri70@gmail.com>
2023-08-20 12:08:55 +0330

    Add project files.

---
+++ ConsoleAppSample/PrintClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSample
+{
+    class PrintClass
+    {
+        public async Task Print(string message)
+        {
+            await Task.Delay(millisecondsDelay: 1000);
+            Console.WriteLine(message);
+        }
+        public static void ParallelPrint(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+}
---
+++ ConsoleAppSample/Program.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleAppSample;
+using System;
+using System.Threading.Tasks;
+
+SampleCode sample = new SampleCode();
+await sample.Print("Hello");
+await sample.Print("World");
+await sample.Print("Ali");
+
+
+
+
+
+///// لطفا نمونه بنویسید که خروجی بالا به صورت Async
+
+Console.WriteLine("Start Async");
+
+PrintClass print = new PrintClass();
+await print.Print("Hello");
+await print.Print("World");
+await print.Print("Ali");
+
+//// لطفا نمونه بنویسید که خروجی بالا به صورت موازی اجرا شود
+
+Console.WriteLine("Start Parallel");
+
+Task printHello = new Task(delegate
+{
+    PrintClass.ParallelPrint("Hello");
+});
+Task printWorld = new Task(delegate
+{
+    PrintClass.ParallelPrint("World");
+});
+Task printAli = new Task(delegate
+{
+    PrintClass.ParallelPrint("Ali");
+});
+
+printHello.Start();
+printWorld.Start();
+printAli.Start();
+
+Console.ReadLine();
